747e03ea82cd8578e5945f2a5f73630a32258609
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-08 21:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,19 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
     if (string.toLowerCase()) {
-      return "I can't hear you!"
-    } else if (string.toUpperCase()) {
-      return "YES INDEED!";
-    } else if (string === "I love you, Grandma.") {
-      return "I love you, too."
+      return "I can't hear you!";
+    }
+}
+      
+function sayHiToGrandma(string){
+  if (string.toUpperCase()) {
+    return "YES INDEED!";
   }
 }
+
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
+
