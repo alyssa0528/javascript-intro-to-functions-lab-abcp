e2190211c54613e53bbde46edc2d42644f995258
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-08 20:46:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
     } else if (string === uppercase) {
       return "yES INDEED!";
-    } else if (string === mixedCase)
+    } else if (string === "I love you, Grandma.") {
+      return 
+    }
   }
 }
